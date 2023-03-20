2ea74bd1784bd8529ba1f63256dd174d0fb872ff
kyncreative <126850167+kyncreative@users.noreply.github.com>
2023-03-20 15:48:19 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,73 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+/*var swiper = new Swiper(".home-slider",{
+   loop:true,
+   navigation:{
+       nextE1: ".swiper-button-next",
+       prevE1: ".swiper-button-prev",
+
+   },
+});*/
+
+var swiper = new Swiper(".home-slider", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+
+var swiper = new Swiper(".reviews-slider", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+
+let loadMoreBtn = document.querySelector('.package .load-more .btn');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll('.package .box-container .box')];
+  for (var i = currentItem; i < currentItem + 3; i++) {
+    boxes[i].style.display = 'inline-block';
+  };
+  currentItem += 3;
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = 'none';
+  }
+}
